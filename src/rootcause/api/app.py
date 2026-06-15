fa0ca748f7ff78a8c60dc56f6c89042d3f294d10
Mouryan-J <mouryan20011002@gmail.com>
2026-06-15 19:31:39 -0400

    Fix CORS: set allow_credentials=False to allow wildcard origin

--- src/rootcause/api/app.py
+++ src/rootcause/api/app.py
@@ -78,7 +78,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
